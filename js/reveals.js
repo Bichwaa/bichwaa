0732e084ccded9bede078aaba277cfb10f7332a1
bichwaa <ptonny13@gmail.com>
2021-06-04 11:43:09 +0300

    added css reveals

--- js/reveals.js
+++ js/reveals.js
@@ -1,3 +1,3 @@
 ScrollReveal().reveal('.section-title', {reset:true});
 
-ScrollReveal().reveal('.about-content', {delay:6500, reset:true});
+ScrollReveal().reveal('.about-content', {delay:300, reset:true});
