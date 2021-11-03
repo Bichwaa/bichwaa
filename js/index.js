11f4f73abd117b5f0ef6b04fcaa14f07e3ecc359
bichwaa <ptonny13@gmail.com>
2021-11-03 08:44:52 +0300

    animations

--- js/index.js
+++ js/index.js
@@ -26,9 +26,9 @@ const words = st.words;
 
 tl.from("header", {yPercent:-100});
 
-tl.to(".header__logo-img", {rotationY: 360*4, duration:1.5, ease:"back", delay:-.5});
+tl.to(".header__logo-img", {rotationY: 360*4, duration:1.5, ease:"back"});
 
-tl.from(".home-hero",{opacity:0.4});
+tl.from(".home-hero",{opacity:0.4, delay:-3});
 
 tl.from(".home-hero__content",{opacity:0, xPercent:-100});
 
@@ -36,8 +36,8 @@ tl.from(".home-hero__socials",{opacity:0});
 
 tl.from(".text-primary",{height:0,fontSize:"0rem", duration:.1, stagger:.75});
 
-tl.staggerFrom(words, .4, {opacity:0, },0.1);
+tl.staggerFrom(words, .05, {opacity:0, },0.05);
 
-tl.from(".btn--bg", {backgroundColor:"black", color:"white", xPercent:20, duration:.5});;
+tl.from(".btn--bg", {backgroundColor:"black", color:"white", xPercent:4, duration:.5});;
 
-// tl.to(".btn--bg", { color:"#9c579a"});
+// tl.to(".heading-sec__main", { color:"#9c579a"});
